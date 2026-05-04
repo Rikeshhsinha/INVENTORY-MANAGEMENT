8037d47d7bd707f9d047c7664644eed40f7b7eba
Rikeshhsinha <rikeshsinha245@gmail.com>
2026-05-04 23:05:45 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      
+     
+     <div className='text-3xl font-bold underline text-red-500'>Hello, Vite + React!</div>
+      
+
+    </div>
+  )
+}
+
+export default App
