8676f11725cdfec2851fffefa1f5ace652774269
Rikeshhsinha <rikeshsinha245@gmail.com>
2026-05-06 22:06:55 +0530

    Project upload

---
+++ Server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './db/connection.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());    
+
+app.listen(process.env.PORT, () => {
+
+    connectDB();
+    
+    console.log('Server is running on http://localhost:' + process.env.PORT);
+});
