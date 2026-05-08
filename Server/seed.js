751d3eb5827abd60db01ab31ba097df1b271d3de
Rikeshhsinha <rikeshsinha245@gmail.com>
2026-05-08 16:21:50 +0530

    Connect server to backend

--- Server/seed.js
+++ Server/seed.js
@@ -1,27 +1,27 @@
-import bycrypt from 'bcryptjs';
-import User from './models/User.js';
-import Post from './models/Post.js';
-import connectDB from './db/connection.js';
+import bcrypt from "bcryptjs";
 
+import User from "./models/User.js";
+import connectDB from "./db/connection.js";
 
-const resister = async () => {
-    try {
-        await connectDB();
-        const hashpassword = await bycrypt.hash("admin", 10);
-        const newUser =new User({
-            name:"Admin",
-            email:"admin@gmail.com",
-            password:hashpassword,
-            role:"admin"
-        })
+const register = async () => {
+  try {
+    await connectDB();
 
-        await newUser.save();
-        console.log("Admin user created successfully");
-    } catch (error) {
-        console.log("Error creating admin user:", error);
-    }   
+    const hashedPassword = await bcrypt.hash("admin", 10);
 
-}
+    const newUser = new User({
+      name: "Admin",
+      email: "admin@gmail.com",
+      password: hashedPassword,
+      role: "admin",
+    });
 
+    await newUser.save();
 
-resister();
+    console.log("Admin user created successfully");
+  } catch (error) {
+    console.log("Error creating admin user:", error);
+  }
+};
+
+register();
